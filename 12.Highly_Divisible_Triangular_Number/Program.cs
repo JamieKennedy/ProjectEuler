3c3914ae19abbf1e7f5e9e82e154e19eb71f72df
Jamie Kennedy <jamiekennedy97@gmail.com>
2020-10-21 12:20:14 +0100

    Completed Euler problem 12, highly divisible triangular numbers

---
+++ 12.Highly_Divisible_Triangular_Number/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12.Highly_Divisible_Triangular_Number {
+    class Program {
+        static void Main(string[] args) {
+            int triangularNumber;
+            int numberOfDivisors;
+
+            const int maxDivisors = 500;
+            int counter = 1;
+
+            while (true) {
+                triangularNumber = GetTriangular(counter);
+                numberOfDivisors = GetNumberOfDivisors(triangularNumber);
+
+                if (numberOfDivisors >= maxDivisors) {
+                    Console.WriteLine($"{triangularNumber} is the first triangular number to have over {maxDivisors} divisors");
+                    break;
+                } else if (numberOfDivisors > 250) {
+                    Console.WriteLine(numberOfDivisors);
+                }
+                counter += 1;
+            }
+        }
+
+        private static int GetTriangular(int num) {
+            int triangularNum = 0;
+
+            for (int i = 1; i <= num; i++) {
+                triangularNum += i;
+            }
+
+            return triangularNum;
+        }
+
+        private static int GetNumberOfDivisors(int num) {
+            int numberOfDivisors = 0;
+            int sqrt = (int)Math.Sqrt(num);
+
+            for (int i = 1; i <= sqrt; i++) {
+                if (num % i == 0) {
+                    numberOfDivisors += 2;
+                }
+            }
+
+            if (sqrt * sqrt == num) {
+                numberOfDivisors--;
+            }
+
+            return numberOfDivisors;
+        }
+
+        private static bool IsEven(int num) {
+            return num % 2 == 0;
+        }
+    }
+}
